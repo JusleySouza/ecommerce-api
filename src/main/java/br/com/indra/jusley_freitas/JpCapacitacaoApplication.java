ca2f4efcb53e6932f785233aef323f2c8c9440ad
Jusley Souza <jusleysouza31@gmail.com>
2026-03-22 11:24:54 -0300

    feat(jp-capacitacao-application) adding Main class

---
+++ src/main/java/br/com/indra/jusley_freitas/JpCapacitacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.indra.jusley_freitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpCapacitacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpCapacitacaoApplication.class, args);
+	}
+
+}
