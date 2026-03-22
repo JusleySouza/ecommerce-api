5dbaca192e135f254a9c97f49e792ab2898ae8b8
Jusley Souza <jusleysouza31@gmail.com>
2026-03-22 13:51:57 -0300

    feat(product-controller) Adding the findProductById route.

--- src/main/java/br/com/indra/jusley_freitas/controller/ProductController.java
+++ src/main/java/br/com/indra/jusley_freitas/controller/ProductController.java
@@ -1,12 +1,15 @@
 package br.com.indra.jusley_freitas.controller;
 
 import br.com.indra.jusley_freitas.dto.request.ProductRequestDTO;
+import br.com.indra.jusley_freitas.dto.response.ProductResponseDTO;
 import br.com.indra.jusley_freitas.model.Product;
 import br.com.indra.jusley_freitas.service.implement.ProductServiceImplement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,11 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody ProductRequestDTO requestDTO){
         return new ResponseEntity<>(service.createProduct(requestDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{productId}")
+    public ResponseEntity<ProductResponseDTO> getProductsById(@PathVariable UUID productId) {
+        return new ResponseEntity<>(service.findProductById(productId), HttpStatus.OK);
     }
 
 }
