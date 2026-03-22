c446d0d3536dbb420baf53891680affaa7585e55
Jusley Souza <jusleysouza31@gmail.com>
2026-03-22 13:36:22 -0300

    feat(product-mapper) Creating the ProductMapper class and mapping the request to the model.

---
+++ src/main/java/br/com/indra/jusley_freitas/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package br.com.indra.jusley_freitas.mapper;
+
+import br.com.indra.jusley_freitas.dto.request.ProductRequestDTO;
+import br.com.indra.jusley_freitas.model.Product;
+
+public class ProductMapper {
+
+    public static Product toModel(ProductRequestDTO dto) {
+        return Product.builder()
+                .name(dto.name())
+                .description(dto.description())
+                .sku(dto.sku())
+                .price(dto.price())
+                .costPrice(dto.costPrice())
+                .stockQuantity(dto.stockQuantity())
+                .active(true)
+                .build();
+    }
+
+}
