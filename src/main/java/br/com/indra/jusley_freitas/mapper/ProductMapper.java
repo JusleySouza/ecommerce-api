94b11ba6a1b8200f8cec9a4dbe1691a92b90ec40
Jusley Souza <jusleysouza31@gmail.com>
2026-03-22 13:55:07 -0300

    feat(product-mapper) adding the toResponse mapping.

--- src/main/java/br/com/indra/jusley_freitas/mapper/ProductMapper.java
+++ src/main/java/br/com/indra/jusley_freitas/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package br.com.indra.jusley_freitas.mapper;
 
 import br.com.indra.jusley_freitas.dto.request.ProductRequestDTO;
+import br.com.indra.jusley_freitas.dto.response.ProductResponseDTO;
 import br.com.indra.jusley_freitas.model.Product;
 
 public class ProductMapper {
@@ -15,6 +16,21 @@ public class ProductMapper {
                 .stockQuantity(dto.stockQuantity())
                 .active(true)
                 .build();
+    }
+
+    public static ProductResponseDTO toResponse(Product entity) {
+        return new ProductResponseDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getSku(),
+                entity.getPrice(),
+                entity.getCostPrice(),
+                entity.getStockQuantity(),
+                entity.getActive(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
     }
 
 }
