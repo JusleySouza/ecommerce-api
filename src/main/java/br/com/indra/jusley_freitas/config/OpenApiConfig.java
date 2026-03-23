00f624b3e760a95cc9716db5f40e43d06042f3f5
Jusley Souza <jusleysouza31@gmail.com>
2026-03-23 11:10:51 -0300

    feat(openapi-config) adding the OpenApiConfig class

---
+++ src/main/java/br/com/indra/jusley_freitas/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package br.com.indra.jusley_freitas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-COMMERCE API")
+                        .description("API for product management")
+                        .version("1.0.0"));
+    }
+
+}
