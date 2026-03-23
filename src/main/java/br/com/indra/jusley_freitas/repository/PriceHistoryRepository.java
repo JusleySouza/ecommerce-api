4563c5be15b931ff1d0b3d7ba6fa1f7abcfc32e2
Jusley Souza <jusleysouza31@gmail.com>
2026-03-23 09:57:54 -0300

    feat(price-history-repository) adding findByProductsId query

--- src/main/java/br/com/indra/jusley_freitas/repository/PriceHistoryRepository.java
+++ src/main/java/br/com/indra/jusley_freitas/repository/PriceHistoryRepository.java
@@ -2,7 +2,14 @@ package br.com.indra.jusley_freitas.repository;
 
 import br.com.indra.jusley_freitas.model.PriceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface PriceHistoryRepository extends JpaRepository<PriceHistory, UUID> {}
+public interface PriceHistoryRepository extends JpaRepository<PriceHistory, UUID> {
+
+    @Query("SELECT h FROM PriceHistory h JOIN FETCH h.product WHERE h.product.id = :productId")
+    List<PriceHistory> findByProductsId(UUID productId);
+
+}
