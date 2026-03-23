ac28b43d73936bc53674eafafe11afcdb6bef52c
Jusley Souza <jusleysouza31@gmail.com>
2026-03-23 19:32:13 -0300

    feat(history-service) Adding validations for existing products and empty product history.

--- src/main/java/br/com/indra/jusley_freitas/service/implement/PriceHistoryServiceImplement.java
+++ src/main/java/br/com/indra/jusley_freitas/service/implement/PriceHistoryServiceImplement.java
@@ -2,9 +2,12 @@ package br.com.indra.jusley_freitas.service.implement;
 
 import br.com.indra.jusley_freitas.config.LoggerConfig;
 import br.com.indra.jusley_freitas.dto.response.HistoryProductResponseDTO;
+import br.com.indra.jusley_freitas.exception.ResourceNotFoundException;
 import br.com.indra.jusley_freitas.mapper.PriceHistoryMapper;
 import br.com.indra.jusley_freitas.model.PriceHistory;
+import br.com.indra.jusley_freitas.model.Product;
 import br.com.indra.jusley_freitas.repository.PriceHistoryRepository;
+import br.com.indra.jusley_freitas.repository.ProductRepository;
 import br.com.indra.jusley_freitas.service.PriceHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,11 +20,19 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PriceHistoryServiceImplement implements PriceHistoryService {
 
+    private final ProductRepository productRepository;
     private final PriceHistoryRepository historyRepository;
 
     @Transactional(readOnly = true)
     public List<HistoryProductResponseDTO> getHistoryByProductId(UUID productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("We were unable to find a product with this ID: " + productId));
+
         List<PriceHistory> priceHistories = historyRepository.findByProductsId(productId);
+
+        if (priceHistories.isEmpty()) {
+            throw new ResourceNotFoundException("No price history found for this product: " + productId);
+        }
 
         LoggerConfig.LOGGER_PRICE_HISTORY.info(" Price history of the successfully returned product!");
         return PriceHistoryMapper.toResponseList(priceHistories);
