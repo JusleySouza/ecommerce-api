1cb1fd37c9c5cf99a361225b27c67869bd334889
Jusley Souza <jusleysouza31@gmail.com>
2026-03-24 19:38:42 -0300

    feat(logger-config) adding subCategory log

--- src/main/java/br/com/indra/jusley_freitas/config/LoggerConfig.java
+++ src/main/java/br/com/indra/jusley_freitas/config/LoggerConfig.java
@@ -4,6 +4,7 @@ import br.com.indra.jusley_freitas.exception.handler.CustomizeResponseEntityExce
 import br.com.indra.jusley_freitas.service.CategoryService;
 import br.com.indra.jusley_freitas.service.PriceHistoryService;
 import br.com.indra.jusley_freitas.service.ProductService;
+import br.com.indra.jusley_freitas.service.SubCategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,7 @@ public class LoggerConfig {
     public static final Logger LOGGER_PRODUCT = LoggerFactory.getLogger(ProductService.class);
     public static final Logger LOGGER_PRICE_HISTORY = LoggerFactory.getLogger(PriceHistoryService.class);
     public static final Logger LOGGER_CATEGORY = LoggerFactory.getLogger(CategoryService.class);
+    public static final Logger LOGGER_SUB_CATEGORY = LoggerFactory.getLogger(SubCategoryService.class);
     public static final Logger LOGGER_EXCEPTION = LoggerFactory.getLogger(CustomizeResponseEntityExceptionHandler.class);
 
 }
