1501a041d0ed140458418ffb18877a74aee74d7b
Jusley Souza <jusleysouza31@gmail.com>
2026-03-24 13:12:30 -0300

    feat(logger-config) adding category log

--- src/main/java/br/com/indra/jusley_freitas/config/LoggerConfig.java
+++ src/main/java/br/com/indra/jusley_freitas/config/LoggerConfig.java
@@ -1,6 +1,7 @@
 package br.com.indra.jusley_freitas.config;
 
 import br.com.indra.jusley_freitas.exception.handler.CustomizeResponseEntityExceptionHandler;
+import br.com.indra.jusley_freitas.service.CategoryService;
 import br.com.indra.jusley_freitas.service.PriceHistoryService;
 import br.com.indra.jusley_freitas.service.ProductService;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ public class LoggerConfig {
 
     public static final Logger LOGGER_PRODUCT = LoggerFactory.getLogger(ProductService.class);
     public static final Logger LOGGER_PRICE_HISTORY = LoggerFactory.getLogger(PriceHistoryService.class);
+    public static final Logger LOGGER_CATEGORY = LoggerFactory.getLogger(CategoryService.class);
     public static final Logger LOGGER_EXCEPTION = LoggerFactory.getLogger(CustomizeResponseEntityExceptionHandler.class);
 
 }
