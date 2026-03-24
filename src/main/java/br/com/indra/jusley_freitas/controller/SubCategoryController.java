08124cb6148f6858df3af15f9e759ba05e88b159
Jusley Souza <jusleysouza31@gmail.com>
2026-03-24 19:41:19 -0300

    feat(subcategory-controller) creation of the SubCategoryController class and adding createSubCategory route

---
+++ src/main/java/br/com/indra/jusley_freitas/controller/SubCategoryController.java
@@ -0,0 +1,30 @@
+package br.com.indra.jusley_freitas.controller;
+
+import br.com.indra.jusley_freitas.dto.request.sub_category.SubCategoryRequestDTO;
+import br.com.indra.jusley_freitas.dto.response.SubCategoryResponseDTO;
+import br.com.indra.jusley_freitas.service.SubCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories/{categoryId}/sub_categories")
+@Tag(name = "SubCategories", description = "Endpoints for sub_category management")
+public class SubCategoryController {
+
+    private final SubCategoryService service;
+
+    @PostMapping
+    @Operation(summary = "Create a sub_category", description = "Create a new sub_category with the provided details.")
+    public ResponseEntity<SubCategoryResponseDTO> createSubCategory(@PathVariable UUID categoryId, @Valid @RequestBody SubCategoryRequestDTO requestDTO){
+        return new ResponseEntity<>(service.createSubCategory(categoryId, requestDTO), HttpStatus.CREATED);
+    }
+
+}
