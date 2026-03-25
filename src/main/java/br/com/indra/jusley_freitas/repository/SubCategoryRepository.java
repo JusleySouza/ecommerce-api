48f81134fba0c13bd9d76cc1117a6e50ee43e315
Jusley Souza <jusleysouza31@gmail.com>
2026-03-24 23:14:56 -0300

    feat(subcategory-repository) adding findByCategoryId query

--- src/main/java/br/com/indra/jusley_freitas/repository/SubCategoryRepository.java
+++ src/main/java/br/com/indra/jusley_freitas/repository/SubCategoryRepository.java
@@ -3,10 +3,13 @@ package br.com.indra.jusley_freitas.repository;
 import br.com.indra.jusley_freitas.model.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, UUID> {
 
     boolean existsByNameAndCategoryId(String name, UUID categoryId);
+
+    List<SubCategory> findByCategoryId(UUID categoryId);
 
 }
