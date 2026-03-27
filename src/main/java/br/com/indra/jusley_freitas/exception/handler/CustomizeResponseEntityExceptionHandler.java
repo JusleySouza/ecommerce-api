a435ea3e19e5e1e096a5897d63a01084b34fac5b
Jusley Souza <jusleysouza31@gmail.com>
2026-03-27 01:44:53 -0300

    feat(exception) adding InsufficientStockException class

--- src/main/java/br/com/indra/jusley_freitas/exception/handler/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/br/com/indra/jusley_freitas/exception/handler/CustomizeResponseEntityExceptionHandler.java
@@ -82,4 +82,12 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
         return new  ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(InsufficientStockException.class)
+    public final ResponseEntity<ExceptionResponse> handleInsufficientStockExceptions(Exception exception, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(), exception.getMessage(), request.getDescription(false));
+        LoggerConfig.LOGGER_EXCEPTION.error(exception.getMessage());
+        return new  ResponseEntity<>(exceptionResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
 }
