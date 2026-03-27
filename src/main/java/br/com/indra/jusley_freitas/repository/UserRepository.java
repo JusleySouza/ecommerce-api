1828a2b59cad9d18ea974a81c5e342bc0714e883
Jusley Souza <jusleysouza31@gmail.com>
2026-03-27 19:28:20 -0300

    feat(user-repository) adding UserRepository interface

---
+++ src/main/java/br/com/indra/jusley_freitas/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package br.com.indra.jusley_freitas.repository;
+
+import br.com.indra.jusley_freitas.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
+
+}
