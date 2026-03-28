8f91039a8b14166aa8e884a69a12670d7b835610
Jusley Souza <jusleysouza31@gmail.com>
2026-03-27 22:05:30 -0300

    feat(cart) adding Cart entity class

---
+++ src/main/java/br/com/indra/jusley_freitas/model/Cart.java
@@ -0,0 +1,36 @@
+package br.com.indra.jusley_freitas.model;
+
+import br.com.indra.jusley_freitas.enums.CartStatus;
+import br.com.indra.jusley_freitas.enums.InventoryReason;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "carts", schema = "JP_CAPACITACAO")
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cart_status", nullable = false)
+    private CartStatus status;
+
+}
