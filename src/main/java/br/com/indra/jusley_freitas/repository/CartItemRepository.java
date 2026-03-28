4c4f8c208a5c51ca040204be7eba382a1ebac154
Jusley Souza <jusleysouza31@gmail.com>
2026-03-27 22:08:32 -0300

    feat(cart-item-repository) adding CartItemRepository interface

---
+++ src/main/java/br/com/indra/jusley_freitas/repository/CartItemRepository.java
@@ -0,0 +1,27 @@
+package br.com.indra.jusley_freitas.repository;
+
+import br.com.indra.jusley_freitas.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
+
+    Optional<CartItem> findByCartIdAndProductId(UUID cartId, UUID productId);
+
+    List<CartItem> findByCartId(UUID cartId);
+
+    @Modifying
+    @Transactional
+    void deleteByCartId(UUID cartId);
+
+    @Modifying
+    @Transactional
+    void deleteByCartIdAndProductId(UUID cartId, UUID productId);
+}
