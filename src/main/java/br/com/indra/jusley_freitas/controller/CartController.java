2d08eedbb6868826c4decf23075fd20b55211b45
Jusley Souza <jusleysouza31@gmail.com>
2026-03-27 22:31:15 -0300

    feat(cart-controller) adding CartController class and getCart route

---
+++ src/main/java/br/com/indra/jusley_freitas/controller/CartController.java
@@ -0,0 +1,31 @@
+package br.com.indra.jusley_freitas.controller;
+
+import br.com.indra.jusley_freitas.dto.request.cart.CartItemRequestDTO;
+import br.com.indra.jusley_freitas.dto.request.cart.UpdateCartItemRequestDTO;
+import br.com.indra.jusley_freitas.dto.response.cart.CartResponseDTO;
+import br.com.indra.jusley_freitas.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users/{userId}/cart")
+@RequiredArgsConstructor
+@Tag(name = "Cart", description = "Endpoints for cart management")
+public class CartController {
+
+    private final CartService cartService;
+
+    @GetMapping
+    @Operation(summary = "Search cart by User ID", description = "Returns the cart associated with the specified User ID.")
+    public ResponseEntity<CartResponseDTO> getCart(@PathVariable UUID userId) {
+        return new ResponseEntity<>(cartService.getCart(userId), HttpStatus.OK);
+    }
+
+}
